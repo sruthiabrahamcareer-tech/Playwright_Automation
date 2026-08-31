b3a8c5f495572588d065fa5a77a37e28094e4ba7
sruthiabrahamcareer-tech <sruthiabrahamcareer@gmail.com>
2026-08-31 13:06:01 -0400

    Updated Playwright tests Aug 31st

--- tests/api/products.api.spec.js
+++ tests/api/products.api.spec.js
@@ -1,9 +1,9 @@
-    const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 const { DemoBlazeApi } = require('../../api/demoblaze.api');
 
 test.describe('DemoBlaze API Tests', () => {
 
-  test('Get product list', async ({ request }) => {
+  test('Get product list', async ({ request }, testInfo) => {
 
     const api = new DemoBlazeApi(request);
 
@@ -12,6 +12,12 @@ test.describe('DemoBlaze API Tests', () => {
     console.log(response);
 
     expect(response.Items.length).toBeGreaterThan(0);
+
+    await testInfo.attach('API Response', {
+      body: JSON.stringify(response, null, 2),
+      contentType: 'application/json'
+    });
+
   });
 
 });
